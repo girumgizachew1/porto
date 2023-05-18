d94790c60acb7b102e48016e6ca8845b19757d16
girum <girumegizachew@gmail.com>
2023-05-18 14:21:13 -0400

    optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { useSpring } from '@react-spring/core'
-import { a } from '@react-spring/web'
-import Overlay from './Overlay'
-import Scene from './Scene'
-
+import React, { useCallback,lazy } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { useSpring } from '@react-spring/core';
+import { a } from '@react-spring/web';
+const Overlay = lazy(() => import('./Overlay'));const Scene = lazy(() => import('./Scene'));
 export default function App() {
   // This spring controls the background and the svg fill (text color)
-  const [{ background, fill }, set] = useSpring({ background: '#f0f0f0', fill: '#202020' }, [])
+  const [{ background, fill }, set] = useSpring({ background: '#f0f0f0', fill: '#202020' }, []);
+
+  // Memoized event handler for OrbitControls
+  const handleControlsChange = useCallback(() => {
+    // Handle controls change here
+  }, []);
+
   return (
     <>
       <a.main style={{ background }}>
         <Canvas className="canvas" dpr={[1, 2]}>
           <Scene setBg={set} />
-          <OrbitControls enablePan={false} enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
+          <OrbitControls
+            enablePan={false}
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+            onChange={handleControlsChange}
+          />
         </Canvas>
         <Overlay fill={fill} />
+    
+        </a.main>
+        <a.main style={{ background }}>
+          <div className="canvas text-red-600 h-screen w-full">
+            projects
+          </div>
 
-
-      </a.main>
-      <div style={{ backgroundcolor:'red', }} className='canvas text-red-600 h-screen w-full' >
-        projects
-      </div>
-    </>
-  )
+        </a.main>
+      </>
+      );
 }
