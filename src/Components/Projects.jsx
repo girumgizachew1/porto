143f5c81af0b8fd00c0cabbbc39430f4737a4992
girum <girumegizachew@gmail.com>
2023-05-19 07:46:21 -0400

    final

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -7,7 +7,7 @@ function Projects({ fill }) {
   const projectList = [
     {
       name: "Watch Config",
-      desktopView: "/WatchDesktop.png",
+      desktopView: "/WatchDesktop.jpg",
       mobileView: '/WatchPhone.jpg',
       performanceScore: '92',
       description: "Watch Config is a 3D watch customizer that allows users to customize and preview a watch in real-time. Users can choose the watch model, color, material, and other specifications to see how the watch will look like before they buy it. The project uses Three.js to create the 3D models and the customizer interface. The development process of Watch Config involved creating a React application that integrates with a Three.js customizer module. The customizer module handles the 3D model creation and manipulation based on user inputs, while the React app manages the state and user interface components.",
@@ -16,7 +16,7 @@ function Projects({ fill }) {
       websiteLink: "watchconfig.vercel.app/"
     }, {
       name: "Ethiopiaye",
-      desktopView: "/EthiopiaDesktop.png",
+      desktopView: "/EthiopiaDesktop.jpg",
       mobileView: '/index.jpg',
       performanceScore: '92',
       description: "Ethiopiaye is a social network for Ethiopian people around the world. The platform allows users to connect with each other, share posts, photos, and videos, and discover events and businesses related to Ethiopian culture. The project aims to promote Ethiopian culture and create a global community of Ethiopians. The development process of Ethiopiaye involved creating a React application that integrates with a Firebase backend. The backend manages the user authentication, data storage, and real-time updates, while the React app manages the user interface and interactions.",
@@ -43,7 +43,7 @@ function Projects({ fill }) {
             <li
               key={index}
               onClick={() => handleProjectClick(project)}
-              className={`project-item text-xl text-white px-10 rounded-sm ${selectedProject && selectedProject.name == project.name ? 'bg-orange-400' : 'bg-orange-300'}`}
+              className={`project-item text-xl text-white px-10 rounded-sm ${selectedProject && selectedProject.name == project.name ? 'text-zinc-800' : 'text-zinc-600'}`}
               
             >
               {project.name}
@@ -56,11 +56,11 @@ function Projects({ fill }) {
         <div fill={fill}>
           {selectedProject ? (
             <div>
-              <h2 className='text-4xl font-bold text-zinc-900' >{selectedProject.name}</h2>
+              <h2 className='text-5xl font-bold text-zinc-900' >{selectedProject.name}</h2>
 
-              <div className='my-4' >
+              <div className='my-4 space-y-4' >
                 <div>
-                  <p  >{selectedProject.description}</p>
+                  <p className='text-sm' >{selectedProject.description}</p>
                   <div className='flex justify-between' >
                     <div>
 
@@ -87,7 +87,7 @@ function Projects({ fill }) {
                 </div>
               </div>
               <div>
-                <p className='bg-orange-400 text-white font-bold text-xl text-right pr-4' >{selectedProject.developmentTechnology}</p>
+                <p className='text-orange-400  font-semibold text-xl text-right pr-4' >{selectedProject.developmentTechnology}</p>
               </div>
             </div>
           ) : (
